6422175703e3f756fd47d76eec635c89d2aa545e
Rostyslav <crnopster@gmail.com>
2020-03-18 12:16:04 +0200

    add Connect to postgres DB

--- storage/postgres.go
+++ storage/postgres.go
@@ -1,2 +1,28 @@
 // Package storage provides postgres database configuration for storing messages.
 package storage
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	connFmt = "host=%v port=%v user=%v password=%v dbname=%v sslmode=disable"
+)
+
+func Connect(psqlHost, psqlPort, psqlUser, psqlPass, psqlDB string) (*sql.DB, error) {
+	connStr := fmt.Sprintf(connFmt, psqlHost, psqlPort, psqlUser, psqlPass, psqlDB)
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
