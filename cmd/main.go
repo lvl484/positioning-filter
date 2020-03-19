4cb2334a51cfe9e9b3cc24ba73e14956a3c24a0f
Kateryna <okkerri@gmail.com>
2020-03-19 08:14:39 +0200

    Delete necessary

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,15 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
+
+	for {
+		log.Println(" [INFO] App is running.")
+		time.Sleep(5 * time.Second)
+	}
 
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/lvl484/positioning-filter
 
 go 1.13
-
-require github.com/gorilla/mux v1.7.4
