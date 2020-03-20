911edd50cf208e777a740d38e0a34016088e72c3
Катя Костюкевич < >
2020-03-20 17:59:30 +0200

    Edit GracefulShutdown()

--- cmd/shutdown.go
+++ cmd/shutdown.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/connect"
+	"github.com/segmentio/kafka-go"
 )
+
+type closer interface {
+	Close() error
+}
+
+type structForClose struct {
+	components []closer
+}
 
 //GracefulShutdown implements releasing all resouces it got from system, finish all request handling and return responses when service stopping.
 func GracefulShutdown(sigs <-chan os.Signal, done chan<- bool) error {
@@ -24,15 +38,44 @@ func GracefulShutdown(sigs <-chan os.Signal, done chan<- bool) error {
 		fmt.Println(ctx.Err())
 	}
 
-	//Replace srv value with your server
-	srv := &http.Server{
+	// Please, replace connection variables with yours components values , or add new.
+	server := &http.Server{
 		Addr: ":8080",
 	}
 
-	defer func() {
-		// Handle extra services here (like closing database, etc.)
+	db, err := sql.Open("driven", "DSN")
+	if err != nil {
+		log.Fatalf("Server shutdown failed:%+v", err)
+	}
 
-		if err := srv.Shutdown(ctx); err != nil {
+	nc, err := net.Dial("tcp", "golang.org:80")
+	if err != nil {
+		log.Fatalf("Server shutdown failed:%+v", err)
+	}
+	kafka := kafka.NewConn(nc, "topic", 0)
+
+	client, _ := api.NewClient(api.DefaultConfig())
+	consul, err := connect.NewService("my-service", client)
+	if err != nil {
+		log.Fatalf("Server shutdown failed:%+v", err)
+	}
+
+	otherComponents := structForClose{
+		components: []closer{
+			//Put connection variables here
+			db,
+			kafka,
+			consul,
+		},
+	}
+
+	defer func() {
+
+		for _, component := range otherComponents.components {
+			component.Close()
+		}
+
+		if err := server.Shutdown(ctx); err != nil {
 			log.Fatalf("Server shutdown failed:%+v", err)
 		}
 
--- go.mod
+++ go.mod
@@ -4,6 +4,10 @@ go 1.13
 
 require (
 	github.com/google/uuid v1.1.1 // indirect
+	github.com/hashicorp/consul v1.7.2
+	github.com/hashicorp/consul/api v1.4.0
+	github.com/hashicorp/vault/api v1.0.4
 	github.com/lib/pq v1.3.0
+	github.com/segmentio/kafka-go v0.3.5
 	github.com/spf13/viper v1.6.2
 )
