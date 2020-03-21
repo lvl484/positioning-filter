73edb1fd6d4abbe4f149d955204278902d8191a3
Rostyslav <crnopster@gmail.com>
2020-03-21 16:32:36 +0200

    add consul auto-deregister

--- consul/agent.go
+++ consul/agent.go
@@ -8,8 +8,9 @@ import (
 )
 
 const (
-	agentCheckInterval = 15
-	agentCheckTimeout  = 5
+	agentDeregisterAfter = 45
+	agentCheckInterval   = 15
+	agentCheckTimeout    = 5
 )
 
 // AgentConfig returns new consul agent config
@@ -18,10 +19,11 @@ func (c *Config) AgentConfig() *consulApi.AgentServiceRegistration {
 		ID:   c.ServiceName,
 		Name: c.ServiceName,
 		Check: &consulApi.AgentServiceCheck{
-			HTTP:     fmt.Sprintf("http://%s:%d%s", c.ServiceName, c.ServicePort, c.ServiceHealthCheckPath),
-			Method:   http.MethodGet,
-			Interval: fmt.Sprintf("%ds", agentCheckInterval),
-			Timeout:  fmt.Sprintf("%ds", agentCheckTimeout),
+			HTTP:                           fmt.Sprintf("http://%s:%d%s", c.ServiceName, c.ServicePort, c.ServiceHealthCheckPath),
+			Method:                         http.MethodGet,
+			Interval:                       fmt.Sprintf("%ds", agentCheckInterval),
+			Timeout:                        fmt.Sprintf("%ds", agentCheckTimeout),
+			DeregisterCriticalServiceAfter: fmt.Sprintf("%ds", agentDeregisterAfter),
 		},
 		Address: c.ServiceName,
 		Port:    c.ServicePort,
