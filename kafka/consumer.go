9198bfaf1e318c7394d4ba078ff58f2650409ca7
Rostyslav <crnopster@gmail.com>
2020-03-23 17:13:31 +0200

    add consumer.go

---
+++ kafka/consumer.go
@@ -0,0 +1,39 @@
+package kafka
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
+type Consumer struct {
+	pc sarama.PartitionConsumer
+}
+
+func NewConsumer(config *Config) (*Consumer, error) {
+	addr := []string{fmt.Sprintf("%v:%v", config.Host, config.Port)}
+	master, err := sarama.NewConsumer(addr, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	consumer, err := master.ConsumePartition(config.ConsumerTopic, config.ConsumerPartition, sarama.OffsetNewest)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{pc: consumer}, nil
+}
+
+func (c Consumer) Consume() {
+	for {
+		select {
+		case err := <-c.pc.Errors():
+			log.Println(err)
+		case msg := <-c.pc.Messages():
+			log.Println(msg)
+		}
+	}
+}
