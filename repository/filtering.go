c546c2bc8b091150d2ead400dced429b703d50ec
alpaka-alpachino <okkerri@gmail.com>
2020-03-28 10:22:53 +0000

    fix funcs, fix interface

--- repository/filtering.go
+++ repository/filtering.go
@@ -3,72 +3,76 @@ package repository
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/google/uuid"
 )
 
-type Postgres struct {
+type filtersRepo struct {
 	db *sql.DB
 }
 
-// NewPostgres returns Postgres with db
-func NewPostgres(db *sql.DB) *Postgres {
-	return &Postgres{db: db}
+type Filters interface {
+	AllByUser(userID uuid.UUID) ([]*Filter, error)
+	Add(filter *Filter) error
+	Update(filter *Filter) error
+	Delete(userID uuid.UUID, name string) error
 }
 
-type FiltersGUD interface {
-	GetFilters() ([]Filter, error)
-	UpdateFilter() error
-	DeleteFilter() error
+func NewFiltersRepository(db *sql.DB) Filters {
+	return &filtersRepo{
+		db: db,
+	}
 }
 
 const (
 	addQuery    = "INSERT INTO FILTERS(name,type,configutation,reversed,user_id) VALUES ($1,$2,$3,$4,$5)"
 	getQuery    = "SELECT * FROM FILTERS WHERE user_id=$1"
 	updateQuery = "UPDATE FILTERS SET (type,configutation,reversed) = ($1,$2,$3) WHERE name = $4"
-	deleteQuery = "DELETE FROM FILTERS WHERE name=$1"
+	deleteQuery = "DELETE FROM FILTERS WHERE user_id=$1 AND name=$2"
 )
 
 // AddFilter adds new filter to database
-func (p *Postgres) AddFilter(filter *Filter) error {
+func (p *filtersRepo) Add(filter *Filter) error {
 	_, err := p.db.Exec(addQuery, filter.Name, filter.Type, filter.Configuration, filter.Reversed, filter.UserID)
 	return err
 }
 
 // GetFilters returns set of filters relevant to user
-func (p *Postgres) GetFilters(userID uuid.UUID) ([]Filter, error) {
-	filtersSet := []Filter{}
-	r, err := p.db.Query(getQuery, userID)
+func (p *filtersRepo) AllByUser(userID uuid.UUID) ([]*Filter, error) {
+	filters := []*Filter{}
 
+	rows, err := p.db.Query(getQuery, userID)
 	if err != nil {
 		return nil, err
 	}
 
-	for r.Next() {
-		f := Filter{}
-		if err := r.Scan(&f); err != nil {
-			continue
+	for rows.Next() {
+		var f *Filter
+		err = rows.Scan(&f.Name, &f.Type, &f.Configuration, &f.Reversed, &f.UserID)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		if err := r.Err(); err != nil {
-			continue
-		}
-
-		filtersSet = append(filtersSet, f)
+		filters = append(filters, f)
 	}
 
-	return filtersSet, nil
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return filters, nil
 }
 
 // UpdateFilter updates filter fields by filter name
-func (p *Postgres) UpdateFilter(filter *Filter) error {
+func (p *filtersRepo) Update(filter *Filter) error {
 	_, err := p.db.Exec(updateQuery, filter.Type, filter.Configuration, filter.Reversed, filter.Name)
 
 	return err
 }
 
 // DeleteFilter deletes filter by name
-func (p *Postgres) DeleteFilter(filterName string) error {
-	_, err := p.db.Exec(deleteQuery, filterName)
+func (p *filtersRepo) Delete(userID uuid.UUID, filterName string) error {
+	_, err := p.db.Exec(deleteQuery, userID, filterName)
 	return err
 }
