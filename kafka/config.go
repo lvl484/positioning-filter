b166202de766a6f33785e1959a9d227159603375
Rostyslav <crnopster@gmail.com>
2020-03-30 00:15:56 +0300

    add comment to kafka config

--- kafka/config.go
+++ kafka/config.go
@@ -1,6 +1,7 @@
 // Package kafka provides producer and consumer to work with kafka topics
 package kafka
 
+// Config for kafka. Contains data to connect and handle messages
 type Config struct {
 	Host            string
 	Port            string
