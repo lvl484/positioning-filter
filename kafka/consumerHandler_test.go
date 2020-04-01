f192e6ce9c53adb43276a6eda2fa467da6707cc8
Rostyslav <crnopster@gmail.com>
2020-04-01 18:51:56 +0300

    update tests for consumerHandler with generated mocks

--- kafka/consumerHandler_test.go
+++ kafka/consumerHandler_test.go
@@ -7,36 +7,33 @@ import (
 	"testing"
 	"time"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	saramaMocks "github.com/Shopify/sarama/mocks"
+	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
-	"github.com/lvl484/positioning-filter/matcher"
-	"github.com/lvl484/positioning-filter/position"
-	"github.com/lvl484/positioning-filter/repository"
 	"github.com/stretchr/testify/assert"
+
+	mockKafka "github.com/lvl484/positioning-filter/kafka/mocks"
+	mockMatcher "github.com/lvl484/positioning-filter/matcher/mocks"
+	"github.com/lvl484/positioning-filter/position"
 )
 
 func TestNewConsumerGroupHandler(t *testing.T) {
-	db, _, err := sqlmock.New()
-	assert.Nil(t, err)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-	filters := repository.NewFiltersRepository(db)
-	matcher := matcher.NewMatcher(filters)
-	saramaProducer := saramaMocks.NewSyncProducer(t, nil)
-	producer := &producer{
-		KafkaProducer: saramaProducer,
-	}
+	producer := mockKafka.NewMockProducer(ctrl)
+	matcher := mockMatcher.NewMockMatcher(ctrl)
+
 	consumerGroupHandler := newConsumerGroupHandler(matcher, producer)
 	assert.NotNil(t, consumerGroupHandler.controller.matcher)
 	assert.NotNil(t, consumerGroupHandler.controller.producer)
 }
 
 func TestMessageControllerHandleMessageSuccess(t *testing.T) {
-	db, _, err := sqlmock.New()
-	assert.Nil(t, err)
+	matcherCtrl := gomock.NewController(t)
+	defer matcherCtrl.Finish()
+	matcher := mockMatcher.NewMockMatcher(matcherCtrl)
 
-	filters := repository.NewFiltersRepository(db)
-	matcher := matcher.NewMatcher(filters)
 	saramaProducer := saramaMocks.NewSyncProducer(t, nil)
 	producer := &producer{
 		KafkaProducer: saramaProducer,
@@ -44,22 +41,19 @@ func TestMessageControllerHandleMessageSuccess(t *testing.T) {
 			ProducerTopic: "filtered-positions",
 		},
 	}
+
 	consumerGroupHandler := newConsumerGroupHandler(matcher, producer)
-
-	var latitude float32 = 1.666
-
-	var longitude float32 = 1.333
 
 	pos := position.Position{
 		UserID:    uuid.New(),
-		Latitude:  latitude,
-		Longitude: longitude,
+		Latitude:  float32(1.111),
+		Longitude: float32(2.222),
 		Timestamp: time.Now(),
 		Arrival:   time.Now().Add(time.Second),
 	}
 
 	b1, err := json.Marshal(pos)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	checker := func(b2 []byte) error {
 		assert.Equal(t, b1, b2)
@@ -68,16 +62,17 @@ func TestMessageControllerHandleMessageSuccess(t *testing.T) {
 
 	saramaProducer.ExpectSendMessageWithCheckerFunctionAndSucceed(checker)
 
+	matcher.EXPECT().Match(pos)
+
 	err = consumerGroupHandler.controller.handleMessage(pos)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 }
 
 func TestMessageControllerHandleMessageFail(t *testing.T) {
-	db, _, err := sqlmock.New()
-	assert.Nil(t, err)
+	matcherCtrl := gomock.NewController(t)
+	defer matcherCtrl.Finish()
+	matcher := mockMatcher.NewMockMatcher(matcherCtrl)
 
-	filters := repository.NewFiltersRepository(db)
-	matcher := matcher.NewMatcher(filters)
 	saramaProducer := saramaMocks.NewSyncProducer(t, nil)
 	producer := &producer{
 		KafkaProducer: saramaProducer,
@@ -85,22 +80,19 @@ func TestMessageControllerHandleMessageFail(t *testing.T) {
 			ProducerTopic: "filtered-positions",
 		},
 	}
+
 	consumerGroupHandler := newConsumerGroupHandler(matcher, producer)
-
-	var latitude float32 = 1.666
-
-	var longitude float32 = 1.333
 
 	pos := position.Position{
 		UserID:    uuid.New(),
-		Latitude:  latitude,
-		Longitude: longitude,
+		Latitude:  float32(1.666),
+		Longitude: float32(1.333),
 		Timestamp: time.Now(),
 		Arrival:   time.Now().Add(time.Second),
 	}
 
 	b1, err := json.Marshal(pos)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	checker := func(b2 []byte) error {
 		assert.Equal(t, b1, b2)
@@ -110,6 +102,8 @@ func TestMessageControllerHandleMessageFail(t *testing.T) {
 	producerError := errors.New("produce message was failed because producer falled asleep")
 
 	saramaProducer.ExpectSendMessageWithCheckerFunctionAndFail(checker, producerError)
+
+	matcher.EXPECT().Match(pos).Return(true, nil)
 
 	err = consumerGroupHandler.controller.handleMessage(pos)
 	assert.EqualError(t, err, "produce message was failed because producer falled asleep")
