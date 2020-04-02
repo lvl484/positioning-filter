eafbebbeacb89d1a6f13cef6580e2712f402ecd6
Rostyslav <crnopster@gmail.com>
2020-04-02 13:31:56 +0300

    remove unused skeleton of matcher

--- matcher/matching.go
+++ matcher/matching.go
@@ -4,23 +4,8 @@ package matcher
 
 import (
 	"github.com/lvl484/positioning-filter/position"
-	"github.com/lvl484/positioning-filter/repository"
 )
 
 type Matcher interface {
 	Match(position.Position) (bool, error)
-}
-
-type matcher struct {
-	filters repository.Filters
-}
-
-func (m *matcher) Match(pos position.Position) (bool, error) {
-	return true, nil
-}
-
-func NewMatcher(filters repository.Filters) Matcher {
-	return &matcher{
-		filters: filters,
-	}
 }
