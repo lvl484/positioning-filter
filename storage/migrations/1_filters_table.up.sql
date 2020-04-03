114ad753c1ba41cc8a8734e95feea0744efe6057
Maksym Bilokur <maksym.bilokur@gmail.com>
2020-04-03 12:16:12 +0300

    refuse migration changes

--- storage/migrations/1_filters_table.up.sql
+++ storage/migrations/1_filters_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS filters (
     type VARCHAR(20) NOT NULL,
     configuration JSON NOT NULL,
     reversed BOOL NOT NULL,
-    user_id UUID NOT NULL
+    user_id VARCHAR(20) NOT NULL
 );
